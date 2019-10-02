ca9ecde140f9abf16a4be0007f384e57a2f0ea2f
matiusgg <newpowerexperience2016@gmail.com>
2019-10-02 16:24:28 +0200

    octubre 02

---
+++ ejercicios/02_Octubre/csrf.py
@@ -0,0 +1,25 @@
+'''
+CSRF(croos-site Request Forgery)
+NOS AYUDA A PROTEGERNOS DE ante la flasificacion de solicitudes desde otros sitios
+Lo que pasa es que pag maliciosas intentan conseguir los datos de una persona por ejemplo de un banco,
+y la persona deja logeado la cuenta, por lo cual estas pag maliciosas recoge estos datos, estos datos se envian como parametros
+mediante las cookies, las cookies no son seguras mientras no se protegan, esto si dejas
+abierto la pestaña del banco y una pagina maliciosa, y esta pagina aprovecha que tienes la pestaña del banco abierta.
+Como es posible, porque como sabes el navegador recopila la informacion del cache.
+
+NPM: eS UN GESTOR DE DEPENEDIAS PARA BULMA-> NODE.JS.
+
+Instalar NOJE:JS-> pones en la consola cmd o en la que quieras npm install bulma
+
+Vas en base.html/layout.html entre el <link> de reset y main.css pones el link de css de bulma:
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+
+y miras la domuntacion en la pagina web para ver como poner css mediante bulma, en donde podemos implementarlos
+en las etiquetas que queramos simplemente poniendo en class="" el nombre de la etiqueta que BULMa te dice
+que pongas para que te haga el css que bulma pone.
+
+
+
+
+'''
---
+++ ejercicios/30_Septiembre/predefinidas.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+funciones predefinidas
+
+*/
+
+//Debug
+
+
+$var="texto de relleno bla, bla.......";
+var_dump($algo);
+print_r($var);
+echo $var;
+print $var;
+
+//fechas
+echo "<hr>";
+echo date('d-m-y';);
+
+
+
+
+?>

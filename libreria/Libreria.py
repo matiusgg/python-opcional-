eef977f1a8c4c0fb502029cf9b88af1190d8a027
matiusgg <newpowerexperience2016@gmail.com>
2019-10-23 16:17:42 +0200

    filtro de busqueda agregado, si son mas de 5 resultados, solo muestrame 5 resultados

--- libreria/Libreria.py
+++ libreria/Libreria.py
@@ -1,4 +1,10 @@
   
+'''
+EN EL CASO DE QUE LA CLASE CONSUMA MUCHO CONTENDIO DE LA CONEXION
+ES MEJOR HACER DOS MODULOS, UNO CON LAS VARIABLES DE LA CONEXION Y OTRO
+CON LA CLASE, Y EL MODULO DE CONEXION LO IMPORTAMOS DENTRO DEL MODULO DE LA CLASE
+ESTO PARA TENERLO MAS CON MODULARIDAD Y QUE NO HALLAN ERRORES, estos en el mismo paquete
+'''
 import pymysql
 
 class Libreria():
@@ -18,6 +24,9 @@ class Libreria():
 
         self.listaLibro = []
 
+        #* Lista solo 5 libros
+        self.lista5Libros = []
+
     def Busqueda(self):
 
         #* ZONA SQL
@@ -34,22 +43,37 @@ class Libreria():
 
         resultado = self.cursor.fetchall()
         
+        if len(resultado) > 5:
 
-        for result in resultado:
+            self.listaLibro.extend([resultado[0],resultado[1], resultado[2], resultado[3], resultado[4]])
 
-            print(result)
+            for i in self.listaLibro:
 
-            autor = result[0]
-            libroAutor = result[1]
-            precio = result[2]
+                self.lista5Libros.extend([i])
 
-            print(autor, libroAutor, precio)
+                print(self.lista5Libros)
 
-            self.listaLibro.extend([[autor, libroAutor, precio]])
+            return self.lista5Libros
 
-            print(self.listaLibro)
 
-        return self.listaLibro
+        else:
+            
+            for result in resultado:
+
+                print(result)
+
+                autor = result[0]
+                libroAutor = result[1]
+                precio = result[2]
+
+                print(autor, libroAutor, precio)
+
+                self.listaLibro.extend([[autor, libroAutor, precio]])
+
+                print(self.listaLibro)
+
+            return self.listaLibro
+
 
         # #* 4. Aceptar/Generar los QUERYS con:
         self.conexion.commit()

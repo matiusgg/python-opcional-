598a17c636f8995abf90841433b3fddf3bc21cb1
matiusgg <newpowerexperience2016@gmail.com>
2019-11-04 16:13:03 +0100

    explicacion ejercicio de cambio de fondo noviembre 04

--- ejercicios/04_noviembre/landingPage.py
+++ ejercicios/04_noviembre/landingPage.py
@@ -1,3 +1,13 @@
 '''
 LANDINGPAGE.
 '''
+
+#* ejercicio con flask en donde cada evz que le demos al boton cambia la imagen y el contenido cambia de color y texto segun la imagen
+#* para ello vamos a usar la libreria DATETIME
+import datetime
+#* Fecha actual
+mes = datetime.datetime.now()
+
+print(mes.year)
+#*%W: SEMANA(week), 0: domingo, 1:lunes, 2:martes, miercoles:3, etc.
+print(mes.strftime('%w'))

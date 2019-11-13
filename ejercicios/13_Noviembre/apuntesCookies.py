701edaa9cfa599c93f15b2efefb968d11f7103d2
matiusgg <newpowerexperience2016@gmail.com>
2019-11-13 16:00:13 +0100

    noviembre 13, explicacion sobre COOKIES, terminado el ejercicio SESSIONREGISTRO y se empezo el ejercicio del ahorcadito en flask

---
+++ ejercicios/13_Noviembre/apuntesCookies.py
@@ -0,0 +1,10 @@
+'''
+IMORTANTE CON LAS COOKIES: Donde crearlas? Es importante que tengamos en cuenta que vamos a crear una cookie cuando vamos a guardar informacion.
+En donde guardaremos esa informacion ya sea por ejemplo un login que guardaran los datos.
+VALIDACIONES: Son las que nos comprueba si los datos son correctos para dejarlos entrar/seguir en el proceso de navegaicond de la pagina.
+Por ejemplo en un login o un registro que valide los datos introducidos para despues guardarlos con la validacion ya hecha. Ademas
+esta validacion debe mantenerse en las demas paginas de la app porque si el usuario pone en la ruta por ejemplo /app con la intencion de 
+saltarse la pagina de login, las validaciones nos permiten evitar que entre sin haber pasado por login porque si intenta hacerlo se expulsara
+porque no hay validaciones.
+Las cookies se crean en un servidor como un entorno virtual por ejemplo. Por lo cual los servidores de Google por ejemplo no tendrian esas cookies.
+'''
